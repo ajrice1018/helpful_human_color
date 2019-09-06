7c9220d82b5520bdae0b96b5cd2acd1fbaf71930
Andrew Rice <ajrice1018@gmail.com>
2019-09-05 22:11:33 -0700

    updated package.json and code clean up

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import SearchBar from './components/navbar';
 import ColorSwatch from './components/colorswatch';
-import SideBar from './components/SideBar';
-import Row from "react-bootstrap/Row";
-import Column from './Column';
-import Container from "./Container";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
       <SearchBar/>  
       <ColorSwatch/>
     </div>
